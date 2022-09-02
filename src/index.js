13cea5c10384e8a4934e13508198ee17e961cc86
Mauro Alfonso <maurogalfonso@gmail.com>
2022-09-02 17:49:56 -0300

    gh pages agregado

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
+// PARA DEPLOY EN GH PAGES COMENTAR BROWSER ROUTER Y DESCOMENTAR HASHROUTER
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <HashRouter> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>
 );
 
